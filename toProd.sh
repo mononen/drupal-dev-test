ae5a95f344d5e200b19b28f4e2467a974b72854b
Adoah <xadoahx@gmail.com>
2021-03-09 21:31:05 -0500

    updating the repo, nothing should've changed

---
+++ toProd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./buildpush.sh
+
+echo "------------------adding latest dev backup to github------------------"
+git add db-backups/backup-latest-dev.sql
+echo "------------------committing to git------------------"
+git commit -m "script automated prod push"
+echo "------------------pushing to git------------------"
+git push
